5d70896b259c019ec3661f9d05f2dd7fab296307
Stanislav <stas-rogachev@mail.ru>
2021-02-23 12:38:20 +0300

    Homework4

---
+++ src/HW4.java
@@ -0,0 +1,108 @@
+import java.util.Scanner;
+
+public class HW4 {
+    public static char[][] map;
+    public static final int SIZE = 3;
+    public static final int DOTS_TO_WIN = 3;
+    public static final char DOTS_EMPTY = '_';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+
+
+
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+        while (true) {
+            playerTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Игрок победил");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            computerTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил компьютер");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+        System.out.println("Игра закончена");
+    }
+
+    public static Scanner in = new Scanner(System.in);
+
+    public static void playerTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты, куда хотите сделать ход. Координаты в виде 'Строка', 'Столбец'");
+            x = in.nextInt() - 1;
+            y = in.nextInt() - 1;
+        } while (!isCellValid(x, y));
+        map[x][y] = DOT_X;
+    }
+
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (map[x][y] == DOTS_EMPTY) return true;
+        return false;
+    }
+
+    public static void computerTurn() {
+        int x, y;
+        do {
+            x = (int) (Math.random() * SIZE);
+            y = (int) (Math.random() * SIZE);
+        } while (!isCellValid(x, y));
+        System.out.println("Компьютер сделал ход в точку " + (x + 1) + " " + (y + 1));
+        map[x][y] = DOT_O;
+    }
+
+    public static boolean checkWin(char symb) {
+        if (map[0][0] == symb && map[0][1] == symb && map[0][2] == symb) return true;
+        if (map[1][0] == symb && map[1][1] == symb && map[1][2] == symb) return true;
+        if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb) return true;
+        if (map[0][0] == symb && map[1][0] == symb && map[2][0] == symb) return true;
+        if (map[0][1] == symb && map[1][1] == symb && map[2][1] == symb) return true;
+        if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb) return true;
+        if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb) return true;
+        if (map[2][0] == symb && map[1][1] == symb && map[0][2] == symb) return true;
+        return false;
+    }
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOTS_EMPTY;
+            }
+
+        }
+    }
+
+    public static void printMap() {
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static boolean isMapFull(){
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOTS_EMPTY) return false;
+            }
+        }
+        return true;
+    }
+} 
